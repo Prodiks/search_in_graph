70f4f363649c8be950ebe7c9d9e7b5bfbf6348c9
Pryaminin <ghzvbyby@gmail.com>
2023-05-31 14:41:42 +0500

    Добавлен базовые обработчики событий пользовательского интерфейса

--- src/main/java/main_app/search_in_graph/MainApp.java
+++ src/main/java/main_app/search_in_graph/MainApp.java
@@ -6,18 +6,22 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import main_app.search_in_graph.MainController;
 
-public class MainApp extends Application {
+public class MainApp extends Application
+{
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException
+    {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("main_view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 440);
-        stage.setTitle("Hello!");
+        stage.setTitle("search_in_graph");
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch();
     }
 }
--- src/main/java/main_app/search_in_graph/MainController.java
+++ src/main/java/main_app/search_in_graph/MainController.java
@@ -1,14 +1,88 @@
 package main_app.search_in_graph;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.input.KeyEvent;
+public class MainController implements Initializable
+{
+//    MainController()
+//    {
+//
+//    }
+    private final int matrix_row_count = 10;
+    private final int matrix_column_count = 10;
 
-public class MainController {
-    @FXML
-    private Label welcomeText;
+    private TextField[][] matrix_fields;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button search_button;
+
+    @FXML
+    private Button clear_button;
+
+    @FXML
+    private GridPane matrix_grid_pane;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        matrix_fields = new TextField[matrix_row_count + 1][matrix_column_count + 1];
+        for (int i = 1; i <= matrix_row_count; i++) {
+            for (int j = 1; j <= matrix_column_count; j++) {
+                TextField text_field = new TextField();
+                text_field.setPrefSize(40, 30);
+
+                matrix_fields[i][j] = text_field;
+                matrix_fields[i][j].setOnKeyTyped(event -> handleMatrixKeyTyped(event, text_field));
+
+                matrix_grid_pane.setMargin(text_field, new Insets(5, 5, 5, 5));
+                matrix_grid_pane.add(text_field, j, i);
+            }
+        }
+    }
+
+    private void handleMatrixKeyTyped(KeyEvent event, TextField textField)
+    {
+        if(textField.getText().startsWith("-")) {
+            textField.setText("-1");
+            System.out.println("axaxa");
+        }
+        System.out.println("heheh " + textField.getText());
+        for (int i = 1; i <= matrix_row_count; i++) {
+            int count_ones = 0;
+            for (int j = 1; j <= matrix_column_count; j++) {
+                if((matrix_fields[i][j].getText() == "1") || (matrix_fields[i][j].getText() == "-1")) {
+                    count_ones++;
+                }
+            }
+            if(count_ones == 2) {
+                for (int j = 1; j <= matrix_column_count; j++) {
+                    if((matrix_fields[i][j].getText() == "1") || (matrix_fields[i][j].getText() == "-1")) {
+                        continue;
+                    }
+                    matrix_fields[i][j].setText("0");
+                }
+            } else if(count_ones > 2) {
+                System.out.println("error");
+            }
+        }
+    }
+
+    @FXML
+    private void handleSearchButtonClick()
+    {
+    }
+
+    @FXML
+    private void handleClearButtonClick()
+    {
+        for (int i = 1; i <= matrix_row_count; i++) {
+            for (int j = 1; j <= matrix_column_count; j++) {
+                matrix_fields[i][j].setText("");
+            }
+        }
     }
 }
